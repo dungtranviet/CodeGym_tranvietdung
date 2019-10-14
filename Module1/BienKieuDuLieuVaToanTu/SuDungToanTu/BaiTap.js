50167d71ea5dcc4651303ea3af8b5324cfefcdc3
dungtranviet <dungtvz91@gmail.com>
2019-10-14 21:36:29 +0700

    'ToanTu'

---
+++ Module1/BienKieuDuLieuVaToanTu/SuDungToanTu/BaiTap.js
@@ -0,0 +1,9 @@
+function DienTich() {
+    let inputWidth=prompt("Enter the Width:");
+    let inputHight=prompt("Enter the Hight:");
+    let Width=parseInt(inputWidth);
+    let Hight=parseInt(inputHight);
+    let area=Width*Hight;
+    return area;
+
+}
