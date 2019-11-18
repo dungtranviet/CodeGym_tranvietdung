781be721c557087d88b5d0ad91f8c40e9fb0e603
dungtranviet <dungtvz91@gmail.com>
2019-11-18 08:33:12 +0700

    'TaoMenu'

---
+++ Module2/src/MangVaPhuongThuc/ThietKeMenuUngDung.java
@@ -0,0 +1,47 @@
+package MangVaPhuongThuc;
+
+import java.util.Scanner;
+
+public class ThietKeMenuUngDung {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int choice;
+        System.out.println("Menu");
+        System.out.println("1.Draw the triangle");
+        System.out.println("2.square");
+        System.out.println("3.rectangle");
+        System.out.println("4.Exit");
+        System.out.println("Enter your choice :");
+        choice=scanner.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Draw the triangle");
+                System.out.println("******");
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+                System.out.println("*");
+                break;
+            case 2:
+                System.out.println("Draw the square");
+                System.out.println("*  *  *  *  *  *");
+                System.out.println("*  *  *  *  *  *");
+                System.out.println("*  *  *  *  *  *");
+                System.out.println("*  *  *  *  *  *");
+                System.out.println("*  *  *  *  *  *");
+                System.out.println("*  *  *  *  *  *");
+                break;
+            case 3:
+                System.out.println("Draw the rectangle");
+                System.out.println("*  *  *  *  *  *");
+                System.out.println("*  *  *  *  *  *");
+                System.out.println("*  *  *  *  *  *");
+                break;
+            case 4:System.exit(0);
+                break;
+            default:
+                System.out.println("No choice");
+        }
+    }
+}
