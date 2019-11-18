647552b8b50ba50e21d6fd7e17912c385bfb1a2e
dungtranviet <dungtvz91@gmail.com>
2019-11-18 14:34:41 +0700

    'TaoLopFan'

---
+++ Module2/src/xay_dung_lop_fan/Fan.java
@@ -0,0 +1,76 @@
+package xay_dung_lop_fan;
+
+public class Fan {
+    int slow=1;
+    int medium=2;
+    int fast=3;
+    int speed=slow;
+    boolean isOn=false;
+    String color="blue";
+    int radius=5;
+    public int getSlow() {
+        return slow;
+    }
+
+    public void setSlow(int slow) {
+        this.slow = slow;
+    }
+
+    public int getMedium() {
+        return medium;
+    }
+
+    public void setMedium(int medium) {
+        this.medium = medium;
+    }
+
+    public int getFast() {
+        return fast;
+    }
+
+    public void setFast(int fast) {
+        this.fast = fast;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        this.isOn = on;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    public Fan(){
+
+    }
+    public String status(){
+        if(this.isOn){
+            return "Fan is On";
+        }
+        else {
+            return "Fan is Off";
+        }
+    }
+    public void setSpeed(int speed){
+        this.speed=speed;
+    }
+    @Override
+    public String toString(){
+        return "Fan : speed ="+this.speed+" ,color: "+color+" ,radius: "+this.radius+" ,"+this.status();
+    }
+}
---
+++ Module2/src/xay_dung_lop_fan/XayDungLopFan.java
@@ -0,0 +1,16 @@
+package xay_dung_lop_fan;
+
+public class XayDungLopFan {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSpeed(fan1.fast);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
+        System.out.println(fan1.toString());
+        Fan fan2 = new Fan();
+        fan2.setSpeed(fan2.medium);
+        fan2.setOn(false);
+        System.out.println(fan2.toString());
+    }
+}
