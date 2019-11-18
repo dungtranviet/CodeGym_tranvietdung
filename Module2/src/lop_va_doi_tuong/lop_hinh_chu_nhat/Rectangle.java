5fd1d15c78fc04f2a521acb3c011ae36d6140311
dungtranviet <dungtvz91@gmail.com>
2019-11-18 11:09:50 +0700

    'LopHinhChuNhat'

---
+++ Module2/src/lop_va_doi_tuong/lop_hinh_chu_nhat/Rectangle.java
@@ -0,0 +1,23 @@
+package lop_va_doi_tuong.lop_hinh_chu_nhat;
+
+public class Rectangle {
+    double width, height;
+
+    public Rectangle() {
+
+    }
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    public double getArea(){
+        return this.width*this.height;
+    }
+    public double getPerimeter(){
+        return (this.width+this.height)*2;
+    }
+    public String display(){
+        return "Rectangle width = "+this.width+", height = "+this.height;
+    }
+
+}
