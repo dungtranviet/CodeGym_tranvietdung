7eb75320d4b5255cbf88184e3c517990914d8506
dungtranviet <dungtvz91@gmail.com>
2019-12-23 16:58:51 +0700

    ''

---
+++ Validation/custom-phone-validation/src/main/java/com/codegym/customphonevalidation/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package com.codegym.customphonevalidation;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("ValidationMessages");
+        return messageSource;
+    }
+}
